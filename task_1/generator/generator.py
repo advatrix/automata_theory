14edba12b58339792495c23f92ac5bea8d9c1219
advatrix <trojan.advatrix@gmail.com>
2020-02-19 21:24:24 +0300

    generator implemention

--- task_1/generator/generator.py
+++ task_1/generator/generator.py
@@ -1,27 +1,49 @@
 import random
-
+import exrex
+import string
 
 
 class Generator:
 
-    def __init__(self, n, accuracy=0.8, target_file='out.txt'):
-        pass
+    def __init__(self, regex, n, target_file='out.txt', accuracy=0.8):
+        self._n = n
+        self._regex = regex
+        self._accuracy = accuracy
+        self._target_file = target_file
 
     def generate(self):
-        '''
-        генерирует строки в количестве n,
-        доля правильных строк - accuracy
-        записывает результат генерации в таргет
-        
-        :return:
-        '''
+        ret = []
+        generator = exrex.generate(self._regex, self._n)
+        for i in range(self._n):
+            tmp_string = generator.__next__()
+            if random.random() > self._accuracy:
+                err_string = self.spoil(tmp_string)
+                ret.append(err_string)
+            else:
+                ret.append(tmp_string)
+        with open(target_file) as f:
+            f.writelines(ret)
 
-    def generate_create(self):
+    @staticmethod
+    def spoil(str_):
+        s = list(str_)
+        n_iterations = random.randint(1, len(s))
+        for i in range(n_iterations):
+            idx = random.randint(0, len(s) + 1)
+            if idx == len(s):
+                s = random.choice(string.printable) + s
+            elif idx == len(s) + 1:
+                s += random.choice(string.printable)
+            else:
+                s[idx] = random.choice(string.printable)
+        return "".join(s)
 
-        '''
 
+if __name__ == '__main__':
+    regex, n, target_file, accuracy = input()
+    n = int(n)
+    accuracy = float(accuracy)
+    random.seed()
+    generator = Generator(regex, n, target_file, accuracy)
+    generator.generate()
 
-
-
-        :return:
-        '''
