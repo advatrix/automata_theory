b07574a6325778f8071ff21c581c1190ffc4c528
advatrix <trojan.advatrix@gmail.com>
2020-02-24 21:57:32 +0300

    fixed regex

--- task_1/regex_analyzer/regex.py
+++ task_1/regex_analyzer/regex.py
@@ -7,8 +7,17 @@ class EntityRedefiningError(RuntimeError):
     pass
 
 
-regex = r'(?i)(create \D[\w\._]*\(\D[\w\._]*(,\D[\w\._]*)*\))|(\D[\w\._]*( join \D[\w\>_]*)?)'
+# r_id = r'create [a-zA-Z\._][a-zA-Z0-9\._]*'
 
+# regex = r'(?i)(create [a-zA-Z\._][a-zA-Z0-9\._]*\([a-zA-Z\._][a-zA-Z0-9\._]*(,[a-zA-Z\._][a-zA-Z0-9\._]*)*\))|([a-zA-Z\._][a-zA-Z0-9\._]*( join [a-zA-Z\._][a-zA-Z0-9\._]*)?)'
+
+# regex = r'(?i)(create {id}\({id}(,{id})*\))|({id}( join {id})?)'.format(id=re.r_id)
+
+
+# regex = r'(?i)(create \D[a-zA-Z0-9\._]*\(\D[a-zA-Z0-9\._]*(,\D[a-zA-Z0-9\._]*)*\))|(\D[a-zA-Z0-9\._]*( join \D[a-zA-Z0-9\._]*)?)'
+
+
+regex = r'(create [a-zA-Z\._][a-zA-Z0-9\._]*\([a-zA-Z\._][a-zA-Z0-9\._]*(,[a-zA-Z\._][a-zA_Z0-9\._]*)*\))|([a-zA-Z\._][a-zA-Z0-9\._]*( join [a-zA-Z\._][a-zA_Z0-9\._]*)?)'
 
 class Storage:
 
@@ -18,11 +27,11 @@ class Storage:
         pass
 
     def _create(self, expr):
-        _expr = cp(expr)
+        _expr = expr[:-1].split('(')
         entity_name = _expr[0]
         if entity_name in self._data:
             raise EntityRedefiningError
-        entity_attrs = _expr[1:-1].split(',')
+        entity_attrs = _expr[1].split(',')
         self._data[entity_name] = entity_attrs
 
     def _output(self, name):
@@ -45,26 +54,29 @@ class Storage:
             ans += list(set0 - set1)
             print(ans)
 
-    def analyze(self, expr):
-        if re.match(expr, regex):
-            _expr = expr.split()
+    def analyze(self, expr_):
+        if re.match(regex, expr_):
+            _expr = expr_.split()
             if _expr[0] == 'create':
                 try:
-                    self._create(_expr[1:])
-                    sys.stderr.write('SUCCESS'+expr)
+                    self._create(_expr[1])
+                    sys.stderr.write('SUCCESS '+expr)
                 except EntityRedefiningError:
-                    sys.stderr.write('ERROR'+expr)
+                    sys.stderr.write('ERROR '+expr)
             else:
                 try:
                     self._output(_expr)
-                    sys.stderr.write('SUCCESS'+expr)
+                    sys.stderr.write('SUCCESS '+expr)
                 except AttributeError:
-                    sys.stderr.write('ERROR'+expr)
+                    sys.stderr.write('ERROR '+expr)
         else:
-            sys.stderr.write('FAIL'+expr)
+            sys.stderr.write('FAIL '+expr)
 
 
 if __name__ == '__main__':
     storage = Storage()
-    for expr in input().split('\n'):
-        storage.analyze(expr)
+    while True:
+        expr = input()
+        print(re.fullmatch(regex, expr))
+        # if expr is not None:
+        #    storage.analyze(expr)
