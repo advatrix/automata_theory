581a026092f15d3d3b4ae5c6b902810fd1f25ad0
advatrix <trojan.advatrix@gmail.com>
2020-02-27 22:51:26 +0300

    smc initial

--- task_1/regex_analyzer/regex.py
+++ task_1/regex_analyzer/regex.py
@@ -1,23 +1,14 @@
 import re
 import sys
-from copy import copy as cp
+from copy import copy
 
 
 class EntityRedefiningError(RuntimeError):
     pass
 
 
-# r_id = r'create [a-zA-Z\._][a-zA-Z0-9\._]*'
-
-# regex = r'(?i)(create [a-zA-Z\._][a-zA-Z0-9\._]*\([a-zA-Z\._][a-zA-Z0-9\._]*(,[a-zA-Z\._][a-zA-Z0-9\._]*)*\))|([a-zA-Z\._][a-zA-Z0-9\._]*( join [a-zA-Z\._][a-zA-Z0-9\._]*)?)'
-
-# regex = r'(?i)(create {id}\({id}(,{id})*\))|({id}( join {id})?)'.format(id=re.r_id)
-
-
-# regex = r'(?i)(create \D[a-zA-Z0-9\._]*\(\D[a-zA-Z0-9\._]*(,\D[a-zA-Z0-9\._]*)*\))|(\D[a-zA-Z0-9\._]*( join \D[a-zA-Z0-9\._]*)?)'
-
-
 regex = r'(create [a-zA-Z\._][a-zA-Z0-9\._]*\([a-zA-Z\._][a-zA-Z0-9\._]*(,[a-zA-Z\._][a-zA_Z0-9\._]*)*\))|([a-zA-Z\._][a-zA-Z0-9\._]*( join [a-zA-Z\._][a-zA_Z0-9\._]*)?)'
+
 
 class Storage:
 
@@ -29,35 +20,39 @@ class Storage:
     def _create(self, expr):
         _expr = expr[:-1].split('(')
         entity_name = _expr[0]
-        if entity_name in self._data:
+        if entity_name in self._data.keys():
             raise EntityRedefiningError
         entity_attrs = _expr[1].split(',')
         self._data[entity_name] = entity_attrs
 
     def _output(self, name):
-        _name = name.split(' join ')
+        _name = copy(name)
         if len(_name) == 1:
-            if name in self._data:
-                print(self._data[name])
+            key = ''.join(_name)
+            if key in self._data.keys():
+                print(self._data[key])
             else:
                 raise AttributeError
         else:
-            if not (_name[0] in self._data and _name[1] in self._data):
+            if not (_name[0] in self._data.keys() and _name[2] in self._data.keys()):
                 raise AttributeError
             ans = []
-            set0 = set(self._data[_name[0]])
-            set1 = set(self._data[_name[1]])
+            key1 = ''.join(_name[0])
+            key2 = ''.join(_name[2])
+            set0 = set(self._data[key1])
+            set1 = set(self._data[key2])
             intersection = list(set0 & set1)
             for item in intersection:
-                ans.append(_name[0]+'.'+item)
-                ans.append(_name[1]+'.'+item)
+                ans.append(key1+'.'+item)
+                ans.append(key2+'.'+item)
             ans += list(set0 - set1)
+            ans += list(set1 - set0)
             print(ans)
 
     def analyze(self, expr_):
-        if re.match(regex, expr_):
+        if re.fullmatch(regex, expr_):
             _expr = expr_.split()
-            if _expr[0] == 'create':
+            if _expr[0] == 'create' and len(_expr) > 1 and '(' in _expr[1]:
                 try:
                     self._create(_expr[1])
                     sys.stderr.write('SUCCESS '+expr)
@@ -73,10 +68,11 @@ class Storage:
             sys.stderr.write('FAIL '+expr)
 
 
+
+
 if __name__ == '__main__':
     storage = Storage()
     while True:
         expr = input()
-        print(re.fullmatch(regex, expr))
-        # if expr is not None:
-        #    storage.analyze(expr)
+        if expr is not None:
+            storage.analyze(expr)
